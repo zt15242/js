8b1ce17bd93b6da35346d60a213cd60dc595e922
zt15242 <74717483+zt15242@users.noreply.github.com>
2021-04-01 08:46:26 +0800

    Update JQuery.js

--- Typing artifact/js/JQuery.js
+++ Typing artifact/js/JQuery.js
@@ -1,16 +1,3 @@
-/*!
- * jQuery JavaScript Library v1.10.0
- * http://jquery.com/
- *
- * Includes Sizzle.js
- * http://sizzlejs.com/
- *
- * Copyright 2005, 2013 jQuery Foundation, Inc. and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2013-05-24T18:39Z
- */
 (function( window, undefined ) {
 
 // Can't do this because several apps including ASP.NET trace
